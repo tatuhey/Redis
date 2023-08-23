a35dca4152be03372d822a5f5a3e346d785aa8de
30065695 <30065695@DELL603931>
2023-08-23 14:32:44 +0800

    Add project files.

---
+++ Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Redis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("South Metropolitan TAFE")]
+[assembly: AssemblyProduct("Redis")]
+[assembly: AssemblyCopyright("Copyright © South Metropolitan TAFE 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b8f77e6-f7a3-4e00-9e16-39b268c7b064")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Redis/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StackExchange.Redis;
+using System.Net;
+
+namespace Redis
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        //private static TestContext testContext = null;
+        //[ClassInitialize]
+
+        //public static void Class_init(TestContext ctx)
+        //{
+        //    testContext = ctx;
+        //}
+
+        [TestMethod]
+        public void TestMethod1_connect()
+        {
+            //var config = "azureredis2023.redis.cache.windows.net:6380,password=ThQaUVV3072JZP1XhFccCVMVwY87FvGiTAzCaBheeiI=,ssl=True,abortConnect=False";
+            //var connMultiplexter = ConnectionMultiplexer.Connect(config);
+            //var redis = connMultiplexter.GetDatabase();
+
+            // Hide the string from the code
+            var ipAddresses = Dns.GetHostAddresses("azureredis2023.redis.cache.windows.net");
+            var port = 6380;
+
+            var config = new ConfigurationOptions
+            {
+                // Endpoints are IP
+                EndPoints = { new IPEndPoint(ipAddresses[0], port) },
+                Ssl = true,
+                Password = "ThQaUVV3072JZP1XhFccCVMVwY87FvGiTAzCaBheeiI=",
+                ClientName = nameof(UnitTest1),
+                AllowAdmin = true, // to have access to all commands
+                AbortOnConnectFail = false
+            };
+
+            var connMultiplexter = ConnectionMultiplexer.Connect(config);
+            var redis = connMultiplexter.GetDatabase();
+
+            //string ourNewConnectionString = testContext.Properties["azureRedisConnectionString"] as string;
+
+            //var connMultiplexter = ConnectionMultiplexer.Connect(ourNewConnectionString);
+            //var redis = connMultiplexter.GetDatabase();
+
+        }
+    }
+}
